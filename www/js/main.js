663500531de84fed5a884064ca6e20a9465b3f19
pojda <thiago.pojda@gmail.com>
2014-03-19 12:01:11 -0300

    single page app

--- www/js/main.js
+++ www/js/main.js
@@ -16,10 +16,8 @@ var app = {
   initialize: function() {
     var self = this;
     this.store = new MemoryStore(function() {
-        self.showAlert('Store Initialized', 'Info');
+        self.renderHomeView();
     });
-    
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
   },
 
   // pretty (native) alert box
@@ -28,8 +26,19 @@ var app = {
       navigator.notification.alert(message, null, title, 'OK');
   } else {
       alert(title ? (title + ": " + message) : message);
-  }
-},
+    }
+  },
+
+  renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+  },
 
 };
 
