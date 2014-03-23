a01a5ce423e6914ec96a06c6180adb765b9b2030
pojda <thiago.pojda@gmail.com>
2014-03-22 23:18:48 -0300

    view class

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,17 @@
 var app = {
 
-  findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-  },
+  // initialize: function() {
+  //   var self = this;
+
+  //   this.store = new MemoryStore(function() {
+  //     $('body').html(new HomeView(self.store).render().el);
+  //   });
+  // },
 
   initialize: function() {
     var self = this;
-
-    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
     this.store = new MemoryStore(function() {
-        self.renderHomeView();
+        $('body').html(new HomeView(self.store).render().el);
     });
   },
 
@@ -26,11 +23,6 @@ var app = {
       alert(title ? (title + ": " + message) : message);
     }
   },
-
-  renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
 
 };
 
